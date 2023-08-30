9d7eb55244993a51dbd763f89d1233178cee4173
Ryan <ryan.lowery@tensoriot.com>
2023-08-30 14:27:24 -0700

    Updated edit profile and profile settings css

--- src/pages/Settings/settings.tsx
+++ src/pages/Settings/settings.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { API, graphqlOperation, Auth } from 'aws-amplify';
-import './settings.css';  // The updated CSS
+import './settings.css';
 import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
 import { getUserProfile } from "../../graphql/queries";
@@ -9,6 +9,9 @@ import { updateUserProfile } from "../../graphql/mutations";
 const Settings: React.FC = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [gender, setGender] = useState("");
   const [address, setAddress] = useState({ street: "", city: "", state: "", country: "", zipCode: "" });
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
@@ -21,6 +24,7 @@ const Settings: React.FC = () => {
         const profile = userProfileData.data.getUserProfile;
         setName(profile.name);
         setEmail(profile.email);
+        setPhone(profile.phone);
         setAddress(profile.address);
       } catch (error) {
         console.log("Error fetching user profile:", error);
@@ -30,10 +34,24 @@ const Settings: React.FC = () => {
     fetchUserProfile();
   }, []);
 
+
+  const handleUpdateAddresses = async () => {
+    const updatedProfile = {
+      id: "userId",
+      address,
+    };
+
+    try {
+      await API.graphql(graphqlOperation(updateUserProfile, { input: updatedProfile }));
+      console.log("Addresses updated.");
+    } catch (error) {
+      console.log("Error updating addresses:", error);
+    }
+  };
+
   const handleUpdateProfile = async () => {
     const updatedProfile = {
       id: "userId",
-      name,
       email,
       address,
     };
@@ -61,27 +79,23 @@ const Settings: React.FC = () => {
       <Header />
       <div className="settings-container">
         <div className="settings-section">
-          <h1>Account Settings</h1>
-          {passwordChanged && <p>Password changed successfully!</p>}
-          <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Name" />
-          <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
-        </div>
-        <div className="settings-section">
           <h2>Manage Addresses</h2>
-          <input type="text" value={address.street} onChange={e => setAddress({ ...address, street: e.target.value })} placeholder="Street" />
-          {/* Add similar input fields for city, state, country, and zipCode */}
-        </div>
-        <div className="settings-section">
-          <h2>Payment Methods</h2>
-          <button onClick={handleUpdateProfile}>Update Profile</button>
+          <form onSubmit={e => { e.preventDefault(); handleUpdateAddresses(); }}>
+            <input type="text" value={address.street} onChange={e => setAddress({...address, street: e.target.value})} placeholder="Street" />
+            <input type="text" value={address.city} onChange={e => setAddress({...address, city: e.target.value})} placeholder="City" />
+            <input type="text" value={address.state} onChange={e => setAddress({...address, state: e.target.value})} placeholder="State" />  {/* New */}
+            <input type="text" value={address.country} onChange={e => setAddress({...address, country: e.target.value})} placeholder="Country" />  {/* New */}
+            <input type="text" value={address.zipCode} onChange={e => setAddress({...address, zipCode: e.target.value})} placeholder="Zip Code" />  {/* New */}
+            <button type="submit">Update Addresses</button>
+          </form>
         </div>
         <div className="settings-section">
           <h2>Change Password</h2>
           <input type="password" value={oldPassword} onChange={e => setOldPassword(e.target.value)} placeholder="Old Password" />
           <input type="password" value={newPassword} onChange={e => setNewPassword(e.target.value)} placeholder="New Password" />
-          <button onClick={handleUpdateProfile}>Update Profile</button>
           <button onClick={handleChangePassword}>Change Password</button>
         </div>
+        {passwordChanged && <p>Password changed successfully!</p>}
       </div>
       <Footer theme="light"/>
     </div>
